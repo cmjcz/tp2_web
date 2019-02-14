0a35c4f17c129a7e80e7cf109617ca8fef4bab8b
vixa <vixa@bsenac.fr>
2019-02-14 15:13:15 -0500

    Ajout selection du compte

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,16 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Comptes;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class LoginController extends AbstractController
 {
     /**
-     * @Route("/", name="login")
+     * @Route("/", name="login", methods={"GET","POST"})
      */
-    public function index()
+    public function index(Request $request)
     {
+
+        $compte = new Comptes();
+        $form = $this->createFormBuilder($compte)->add('nom', TextType::class)->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $compte = $this->getDoctrine()->getRepository(Comptes::class)
+            ->findOneBy(array('nom' => $compte->getNom()));
+            if(!is_null($compte)){
+                return $this->redirectToRoute('comptes_show', ['idcomptes' => $compte->getIdcomptes()]);
+            }
+        }
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'form' => $form->createView(),
         ]);
     }
 }

15659dbf704f61cd651c6653dc1603779a6f1084
vixa <vixa@bsenac.fr>
2019-02-17 17:07:24 -0500

    Ajout dépot et retrait

---
+++ src/Controller/ChangeMoneyController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use \DateTime;
+use App\Entity\Comptes;
+use App\Entity\Transactions;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ChangeMoneyController extends AbstractController
+{
+    /**
+     * @Route("/retrait/{compte}", name="retrait")
+     */
+    public function retrait(Request $request, Comptes $compte) : Response
+    {
+        return $this->changeMonney($compte, $request, -1, 'Retrait');
+    }
+
+    /**
+     * @Route("/depot/{compte}", name="depot")
+     */
+    public function depot(Request $request, Comptes $compte) : Response
+    {
+        return $this->changeMonney($compte, $request, 1, 'Depot');
+    }
+
+    private function changeMonney(Comptes $compte, Request $request, int $signe, String $nom) : Response
+    {
+        $transaction = new Transactions();
+        $form = $this->createFormBuilder($transaction)->add('solde', NumberType::class)->getForm();
+        $form->handleRequest($request);
+        $transaction->setDate(new DateTime())->setIdCompte($compte);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $transaction->setSolde($transaction->getSolde() * $signe);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($transaction);
+            $entityManager->flush();
+        }
+
+        return $this->render('change_money/index.html.twig', [
+            'operation' => $nom,
+            'form' => $form->createView(),
+        ]);
+    }
+}
